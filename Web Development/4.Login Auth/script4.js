e6ecd690d73394c332ba08391c019e3859437b27
PradeepKumar094 <durgaramasatya90@gmail.com>
2024-08-20 19:35:01 +0530

    Add files via upload

---
+++ Web Development/4.Login Auth/script4.js
@@ -0,0 +1,38 @@
+var users = [ ];
+function register() {
+  var username = document.getElementById('registerUsername').value;
+  var password = document.getElementById('registerPassword').value;
+
+  if (users.some(user => user.username === username)) {
+    window.alert('Username already exists. Please choose another one.');
+    return;
+  }
+  users.push({ username: username, password: password });
+  window.alert('Registration successful! Please log in.');
+  showLoginForm();
+}
+function login() {
+  var username = document.getElementById('loginUsername').value;
+  var password = document.getElementById('loginPassword').value;
+
+  var user = users.find(user => user.username === username && user.password === password);
+
+  if (user) {
+    window.alert('Login successful!');
+    showSecuredPage();
+  } else {
+    window.alert('Invalid username or password. Please try again.');
+  }
+}
+
+function showLoginForm() {
+  document.getElementById('loginForm').style.display = 'block';
+  document.getElementById('registerForm').style.display = 'none';
+  document.getElementById('securedPage').style.display = 'none';
+}
+
+function showSecuredPage() {
+  document.getElementById('loginForm').style.display = 'login successfull';
+  document.getElementById('registerForm').style.display = 'none';
+  document.getElementById('securedPage').style.display = 'block';
+}
