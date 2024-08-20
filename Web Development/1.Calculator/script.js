d0639117d843ff6bc3c0072875b8f0797317a54d
PradeepKumar094 <durgaramasatya90@gmail.com>
2024-08-20 19:30:22 +0530

    Add files via upload

---
+++ Web Development/1.Calculator/script.js
@@ -0,0 +1,16 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+function calculate() {
+    var displayValue = document.getElementById('display').value;
+    var result;
+    try {
+      result = eval(displayValue); 
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+}
